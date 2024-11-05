efafb66dad389ce03a2adb469e3dd819cea1f095
Le_PC_qui_parle <nicolas.cleuvenotbilariki@gmail.com>
2024-11-05 17:47:39 +0100

    script

--- DrawingEditor.py
+++ DrawingEditor.py
@@ -2,8 +2,8 @@ from turtle import *
 from kandinsky import draw_string as drawTxt, fill_rect as drawRect
 from ion import *
 
-BLACK,WHITE,GRAY,RED,GREEN,BLUE,YELLOW,PURPLE,ORANGE=(0,0,0),(255,255,255),(127,127,127),(240,10,10),(10,240,10),(10,10,240),(240,240,10),(240,10,240),(240,150,0)
-coul=BLACK
+NOIR,BLANC,GRIS,ROUGE,VERT,BLEU,JAUNE,MAGENTA,ORANGE=(0,0,0),(255,255,255),(127,127,127),(240,10,10),(10,240,10),(10,10,240),(240,240,10),(240,10,240),(240,150,0)
+coul=NOIR
 psize=1
 def k(k): return keydown(k)
 def init():
@@ -19,10 +19,10 @@ def splash(c):
   pensize(psize)
   goto(position())
   penup()
-  color(BLACK)
+  color(NOIR)
 def ifsplash(c):
   if k(KEY_OK): splash(c)
-def moove():
+def mouv():
   if k(KEY_UP):
     goto(position()[0],position()[1]+1)
   if k(KEY_DOWN):
@@ -33,30 +33,28 @@ def moove():
     goto(position()[0]-1,position()[1])
 def pSizeSwitch():
   global psize
-  if k(KEY_ZERO):
-    if psize==1:
-      pensize(25)
-      psize=25
-    else:
-      pensize(1)
-      psize=1
-    while k(KEY_ZERO): continue
+  if k(KEY_PLUS):
+    psize+=1
+    while k(KEY_PLUS): continue
+  if k(KEY_MINUS):
+    psize-=1
+    while k(KEY_MINUS): continue
 def coulSwitch():
   global coul
-  if k(KEY_ONE):coul=BLACK
-  if k(KEY_TWO):coul=WHITE
-  if k(KEY_THREE):coul=GRAY
-  if k(KEY_FOUR):coul=RED
-  if k(KEY_FIVE):coul=GREEN
-  if k(KEY_SIX):coul=BLUE
-  if k(KEY_SEVEN):coul=YELLOW
-  if k(KEY_EIGHT):coul=PURPLE
+  if k(KEY_ONE):coul=NOIR
+  if k(KEY_TWO):coul=BLANC
+  if k(KEY_THREE):coul=GRIS
+  if k(KEY_FOUR):coul=ROUGE
+  if k(KEY_FIVE):coul=VERT
+  if k(KEY_SIX):coul=BLEU
+  if k(KEY_SEVEN):coul=JAUNE
+  if k(KEY_EIGHT):coul=MAGENTA
   if k(KEY_NINE):coul=ORANGE
-def drawCol():
+def drawColor():
   drawTxt("color:",5,5)
   drawRect(80,5,20,20,coul)
 def drawSize():
-  drawTxt("       ",120,5)
+  drawTxt("            ",120,5)
   drawTxt("size:"+str(psize),120,5)
 def ifInit():
   if keydown(KEY_EXE):
@@ -65,9 +63,9 @@ def ifInit():
 init()
 while 1:
   ifInit()
-  moove()
+  mouv()
   pSizeSwitch()
   coulSwitch()
   ifsplash(coul)
-  drawCol()
+  drawColor()
   drawSize()
