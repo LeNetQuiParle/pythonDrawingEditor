09caf2307a86a02f7b66af61ed55e7584c4879f2
Le_PC_qui_parle <nicolas.cleuvenotbilariki@gmail.com>
2024-11-05 08:18:13 +0100

    script

--- DrawingEditor.py
+++ DrawingEditor.py
@@ -2,8 +2,8 @@ from turtle import *
 from kandinsky import draw_string as drawTxt, fill_rect as drawRect
 from ion import *
 
-NOIR,BLANC,GRIS,ROUGE,VERT,BLEU,JAUNE,MAGENTA,ORANGE=(0,0,0),(255,255,255),(127,127,127),(240,10,10),(10,240,10),(10,10,240),(240,240,10),(240,10,240),(240,150,0)
-coul=NOIR
+BLACK,WHITE,GRAY,RED,GREEN,BLUE,YELLOW,PURPLE,ORANGE=(0,0,0),(255,255,255),(127,127,127),(240,10,10),(10,240,10),(10,10,240),(240,240,10),(240,10,240),(240,150,0)
+coul=BLACK
 psize=1
 def k(k): return keydown(k)
 def init():
@@ -19,7 +19,7 @@ def splash(c):
   pensize(psize)
   goto(position())
   penup()
-  color(NOIR)
+  color(BLACK)
 def ifsplash(c):
   if k(KEY_OK): splash(c)
 def moove():
@@ -43,18 +43,18 @@ def pSizeSwitch():
     while k(KEY_ZERO): continue
 def coulSwitch():
   global coul
-  if k(KEY_ONE):coul=NOIR
-  if k(KEY_TWO):coul=BLANC
-  if k(KEY_THREE):coul=GRIS
-  if k(KEY_FOUR):coul=ROUGE
-  if k(KEY_FIVE):coul=VERT
-  if k(KEY_SIX):coul=BLEU
-  if k(KEY_SEVEN):coul=JAUNE
-  if k(KEY_EIGHT):coul=MAGENTA
+  if k(KEY_ONE):coul=BLACK
+  if k(KEY_TWO):coul=WHITE
+  if k(KEY_THREE):coul=GRAY
+  if k(KEY_FOUR):coul=RED
+  if k(KEY_FIVE):coul=GREEN
+  if k(KEY_SIX):coul=BLUE
+  if k(KEY_SEVEN):coul=YELLOW
+  if k(KEY_EIGHT):coul=PURPLE
   if k(KEY_NINE):coul=ORANGE
 def drawCol():
   drawTxt("color:",5,5)
-  drawRect(90,5,20,20,coul)
+  drawRect(80,5,20,20,coul)
 def drawSize():
   drawTxt("       ",120,5)
   drawTxt("size:"+str(psize),120,5)
